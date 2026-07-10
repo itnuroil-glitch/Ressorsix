8429d11147fbd91d55885575dd47aa3d13a4ac63
unknown <vishnupriyaashish9624@gmail.com>
2026-07-10 12:15:59 +0400

    10-07-2026

--- backend/src/routes/purchaseRoutes.js
+++ backend/src/routes/purchaseRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const PurchaseController = require('../controllers/PurchaseController');
+const PurchaseController = require('../controllers/purchaseController');
 
 router.get('/', PurchaseController.getAllPurchases);
 router.post('/', PurchaseController.createPurchase);
